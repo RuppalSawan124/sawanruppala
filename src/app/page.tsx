51e782abcb116fc50d7c7078d2e96ca8024e34af
RuppalSawan124 <ruppalasawan@gmail.com>
2024-09-20 11:07:45 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ import Skills from './components/skills';
 import AboutMe from './components/aboutme';
 import Project from './components/projects';
 import { motion } from 'framer-motion';
-import { FaGithub, FaLinkedin,FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import { BiSolidNotepad } from "react-icons/bi";
 import dynamic from 'next/dynamic';
 import ReactCursorPosition from 'react-animated-cursor';
+import Head from 'next/head';
 
 const ModelViewer = dynamic(() => import('./components/modelviewer'), {
   ssr: false,
@@ -18,134 +19,135 @@ const ModelViewer = dynamic(() => import('./components/modelviewer'), {
 export default function Home() {
   return (
     <div className="bg-[#0a0a0a] text-white font-sans min-h-screen">
-       <ReactCursorPosition color='255,255,255'/>
-      <Analytics/>
+        <title>Sawan Ruppala</title>
+      <ReactCursorPosition color='255,255,255' />
+      <Analytics />
       {/* Navbar */}
       <div className="bg-gradient-to-tr from-[#0a0a0a] via-[#0a0a0a] to-gray-700 bg-[length:200%_200%]" >
-      <Navbar />
+        <Navbar />
 
-      {/* Main Section */}
-      <main className="flex flex-col md:flex-row justify-between px-10 py-32 pt-20 md:px-10">
-        {/* Left content */}
-        <div className="flex flex-col items-center md:items-start justify-center w-full md:w-1/2 text-center md:text-left">
-          <motion.h1
-            className="text-4xl md:text-5xl font-bold mb-8"
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.3, delay:1, ease: 'easeOut' }}
-          >
-            Welcome To My Website
-          </motion.h1>
+        {/* Main Section */}
+        <main className="flex flex-col md:flex-row justify-between px-10 py-32 pt-20 md:px-10">
+          {/* Left content */}
+          <div className="flex flex-col items-center md:items-start justify-center w-full md:w-1/2 text-center md:text-left">
+            <motion.h1
+              className="text-4xl md:text-5xl font-bold mb-8"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.3, delay: 1.5, ease: 'easeOut' }}
+            >
+              Welcome To My Website
+            </motion.h1>
 
-          <motion.p
-            className="text-xl mb-4 font-extralight"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1.5, ease: 'easeOut' }}
-          >
-            I'm Sawan Ruppala - a junior web designer at Pacific Cellulose LTD
-          </motion.p>
+            <motion.p
+              className="text-xl mb-4 font-extralight"
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 2, ease: 'easeOut' }}
+            >
+              I'm Sawan Ruppala - a junior web designer at Pacific Cellulose LTD
+            </motion.p>
 
-          {/* Social Icons Animation */}
+            {/* Social Icons Animation */}
+            <motion.div
+              className="flex space-x-8 md:space-x-20 mt-4 justify-center md:justify-start"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1, delay: 2.4, ease: 'easeOut' }}
+            >
+              {/* Social icons with hover effects */}
+              <motion.a
+                href="https://github.com/RuppalSawan124"
+                className="text-gray-400 hover:text-white"
+                aria-label="GitHub"
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{
+                  x: [0, -3, 3, -3, 3, 0],
+                  y: [0, -2, 2, -2, 2, 0],
+                  transition: {
+                    duration: 0.4,
+                    repeat: Infinity,
+                    repeatType: 'loop',
+                    ease: 'easeInOut',
+                  },
+                }}
+              >
+                <FaGithub size={40} className="md:text-3xl" />
+              </motion.a>
+
+              <motion.a
+                href="https://www.linkedin.com/in/sawan-ruppala-304623235"
+                className="text-gray-400 hover:text-white"
+                aria-label="LinkedIn"
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{
+                  x: [0, -3, 3, -3, 3, 0],
+                  y: [0, -2, 2, -2, 2, 0],
+                  transition: {
+                    duration: 0.4,
+                    repeat: Infinity,
+                    repeatType: 'loop',
+                    ease: 'easeInOut',
+                  },
+                }}
+              >
+                <FaLinkedin size={40} className="md:text-3xl" />
+              </motion.a>
+
+              <motion.a
+                href="https://docs.google.com/document/d/1s8yTa3m1l-i8k055LQT-aqud0f18Ro8gA9sjJP8Qm5g/edit?usp=sharing"
+                className="text-gray-400 hover:text-white"
+                target="_blank"
+                aria-label="Resume"
+                whileHover={{
+                  x: [0, -3, 3, -3, 3, 0],
+                  y: [0, -2, 2, -2, 2, 0],
+                  transition: {
+                    duration: 0.4,
+                    repeat: Infinity,
+                    repeatType: 'loop',
+                    ease: 'easeInOut',
+                  },
+                }}
+              >
+                <BiSolidNotepad size={40} className="md:text-3xl" />
+              </motion.a>
+
+              <motion.a
+                href="mailto:ruppalasawan@gmail.com"
+                className="text-gray-400 hover:text-white"
+                aria-label="Mail"
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{
+                  x: [0, -3, 3, -3, 3, 0],
+                  y: [0, -2, 2, -2, 2, 0],
+                  transition: {
+                    duration: 0.4,
+                    repeat: Infinity,
+                    repeatType: 'loop',
+                    ease: 'easeInOut',
+                  },
+                }}
+              >
+                <FaEnvelope size={40} className="md:text-3xl" />
+              </motion.a>
+            </motion.div>
+          </div>
+
+
+          {/* Right side for the GLTF Model */}
           <motion.div
-            className="flex space-x-8 md:space-x-20 mt-4 justify-center md:justify-start"
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1, delay: 2, ease: 'easeOut' }}
+            className="flex w-full md:w-1/2 h-60 md:h-96"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 4, delay: 1.5 }}
           >
-            {/* Social icons with hover effects */}
-            <motion.a
-              href="https://github.com/RuppalSawan124"
-              className="text-gray-400 hover:text-white"
-              aria-label="GitHub"
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{
-                x: [0, -3, 3, -3, 3, 0],
-                y: [0, -2, 2, -2, 2, 0],
-                transition: {
-                  duration: 0.4,
-                  repeat: Infinity,
-                  repeatType: 'loop',
-                  ease: 'easeInOut',
-                },
-              }}
-            >
-              <FaGithub size={40} className="md:text-3xl" />
-            </motion.a>
-
-            <motion.a
-              href="https://www.linkedin.com/in/sawan-ruppala-304623235"
-              className="text-gray-400 hover:text-white"
-              aria-label="LinkedIn"
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{
-                x: [0, -3, 3, -3, 3, 0],
-                y: [0, -2, 2, -2, 2, 0],
-                transition: {
-                  duration: 0.4,
-                  repeat: Infinity,
-                  repeatType: 'loop',
-                  ease: 'easeInOut',
-                },
-              }}
-            >
-              <FaLinkedin size={40} className="md:text-3xl" />
-            </motion.a>
-
-            <motion.a
-              href="https://docs.google.com/document/d/1s8yTa3m1l-i8k055LQT-aqud0f18Ro8gA9sjJP8Qm5g/edit?usp=sharing"
-              className="text-gray-400 hover:text-white"
-              target="_blank"
-              aria-label="Resume"
-              whileHover={{
-                x: [0, -3, 3, -3, 3, 0],
-                y: [0, -2, 2, -2, 2, 0],
-                transition: {
-                  duration: 0.4,
-                  repeat: Infinity,
-                  repeatType: 'loop',
-                  ease: 'easeInOut',
-                },
-              }}
-            >
-              <BiSolidNotepad size={40} className="md:text-3xl" />
-            </motion.a>
-
-            <motion.a
-               href="mailto:ruppalasawan@gmail.com"
-              className="text-gray-400 hover:text-white"
-              aria-label="Mail"
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{
-                x: [0, -3, 3, -3, 3, 0],
-                y: [0, -2, 2, -2, 2, 0],
-                transition: {
-                  duration: 0.4,
-                  repeat: Infinity,
-                  repeatType: 'loop',
-                  ease: 'easeInOut',
-                },
-              }}
-            >
-              <FaEnvelope size={40} className="md:text-3xl" />
-            </motion.a>
+            <ModelViewer />
           </motion.div>
-        </div>
-        
-
-        {/* Right side for the GLTF Model */}
-        <motion.div
-          className="flex w-full md:w-1/2 h-60 md:h-96"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 4, delay:1.5 }}
-        >
-          <ModelViewer />
-        </motion.div>
-      </main>
+        </main>
       </div>
 
       <AboutMe />
